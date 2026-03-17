d297962e7536955ed4ae6b071e8ed68355bbb782
Simpleshaikh1 <hamzatbolaji648@yahoo.com>
2026-03-17 18:37:44 +0100

    SimpleStorage Solidity smart contract.

---
+++ SimpleStorage.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18; //solidty version
+
+contract SimpleStorage {
+    //favoriteNumber gets initialized to 0 if no value is given.
+
+    uint256 myFavoriteNumber; //0
+
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    //dynamic array
+    Person[] public listOfPeople; //[]
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push( Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
